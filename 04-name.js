fc14957faf4d83a89304ec9702a524cc651f7a1c
My Name <191200107035@asoit.edu.in>
2023-09-07 17:48:41 +0530

    first day learning

---
+++ 04-name.js
@@ -0,0 +1,5 @@
+const john = "john";
+const peter = "peter";
+const secret = "SUPER SECRET";
+console.log(module);
+module.exports = { john, peter };
---
+++ modules.js
@@ -0,0 +1,12 @@
+// Modules
+// commonJs , every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const name = require("./04-name");
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-filters");
+require("./07-mind-grenade");
+// console.log(data);
+// console.log(name);
+// sayHi("ayush");
+// sayHi(name.john);
+// sayHi(name.peter);
